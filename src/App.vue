b1b7a2841497d6022cb65ff4a2b76c2fff877303
skycity233 <544907049@qq.com>
2019-12-18 16:10:56 +0800

    :lipstick:

--- src/App.vue
+++ src/App.vue
@@ -1,317 +1,331 @@
-<template>
-  <div id="app">
-    <vue-particles
-      class="particles"
-      color="#0f4c81"
-      :particleOpacity="0.3"
-      :particlesNumber="20"
-      shapeType="circle"
-      :particleSize="4"
-      :lineLinked="false"
-      :moveSpeed="3"
-      :hoverEffect="false"
-      :clickEffect="false"
-    />
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+    <div id="app">
+        <vue-particles
+                class="particles"
+                color="#0f4c81"
+                :particleOpacity="0.3"
+                :particlesNumber="20"
+                shapeType="circle"
+                :particleSize="4"
+                :lineLinked="false"
+                :moveSpeed="3"
+                :hoverEffect="false"
+                :clickEffect="false"
+        />
 
-    <vue-navigation-bar :options="navbarOptions">
-      <template v-slot:custom-section>
-        <div class="link">
-          <router-link to="/">HOME</router-link>
-        </div>
-        <div class="link">
-          <router-link to="/about">ABOUT</router-link>
-        </div>
-        <div class="link">
-          <router-link to="/subscribe">SUBSCRIBE</router-link>
-        </div>
-      </template>
-    </vue-navigation-bar>
+        <vue-navigation-bar :options="navbarOptions">
+            <template v-slot:custom-section>
+                <div class="link">
+                    <router-link to="/">HOME</router-link>
+                </div>
+                <div class="link">
+                    <router-link to="/about">ABOUT</router-link>
+                </div>
+                <div class="link">
+                    <router-link to="/subscribe">SUBSCRIBE</router-link>
+                </div>
+            </template>
+        </vue-navigation-bar>
 
-    <transition enter-active-class="animation-fade-in">
-      <router-view />
-    </transition>
+        <transition enter-active-class="animation-fade-in">
+            <router-view/>
+        </transition>
 
-    <div id="footer">
-      <div id="footer-text">
-        <p>
-          Built with ♥ and
-          <a href="https://vuejs.org/">Vue.js</a> | CSS animations by
-          <a href="http://animista.net/">Animista</a> | Proudly deployed on
-          <a href="https://netlify.com/">Netlify</a> 
-		 
-        </p>
-		
-        <p id="busuanzi_container_site_pv">
-          Viewed by you guys for a total of
-          <span id="busuanzi_value_site_pv"></span> times ♥
-        </p>
-		<p 
-		<a href="https://github.com/spencerwooo/portfolio">
-			<img src="https://flat.badgen.net/badge/Designed%20by/@SpencerWoo/blue" alt="copyright" />
-		</a>
-		
-		</p>
-		<div style="float:left;width:450px;">Last updated on 17 Dec. 2019</div>
-        <p id="footer-copyright">©{{ getYear() }} Jack Wang</p>
-		
-		
-      </div>
+        <div id="footer">
+            <div id="footer-text">
+                <p>
+                    Built with ♥ and
+                    <a href="https://vuejs.org/">Vue.js</a> | CSS animations by
+                    <a href="http://animista.net/">Animista</a> | Proudly deployed on
+                    <a href="https://netlify.com/">Netlify</a>
+
+                </p>
+
+
+                <p id="busuanzi_container_site_pv">
+                    Viewed by
+                    <span id="busuanzi_value_site_uv"></span>
+                    you of guys for a total of
+                    <span id="busuanzi_value_site_pv"></span> times ♥
+
+
+                </p>
+
+
+                <p>
+                <a href="https://github.com/spencerwooo/portfolio">
+                    <img src="https://flat.badgen.net/badge/Designed%20by/@SpencerWoo/blue" alt="copyright"/>
+                </a>
+
+                    <span style="font-size:1px ;padding: 30px">Hey Spencer, Don't look around, I'm Here, Your badged, I just changed a blue shirt</span>
+
+
+                </p>
+
+                <p id="footer-copyright">©{{ getYear() }} Jack Wang
+                </p>
+
+
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
+
 <script>
-import 'vue-navigation-bar/dist/vue-navigation-bar.css'
+    import 'vue-navigation-bar/dist/vue-navigation-bar.css'
 
-export default {
-  name: 'app',
+    export default {
+        name: 'app',
 
-  data() {
-    return {
-      navbarOptions: {
-        elementId: 'main-navbar',
-        mobileBreakpoint: 992,
-        ariaLabelMainNav: 'Main Navigation',
-        menuOptionsRight: [
-          {
-            type: 'button',
-            text: '🌟 ME ON GITHUB',
-            path: 'https://github.com/spencerwooo/portfolio',
-            class: 'button-github'
-          }
-        ]
-      }
+        data() {
+            return {
+                navbarOptions: {
+                    elementId: 'main-navbar',
+                    mobileBreakpoint: 992,
+                    ariaLabelMainNav: 'Main Navigation',
+                    menuOptionsRight: [
+                        {
+                            type: 'button',
+                            text: '🌟 Designer\'s GITHUB',
+                            path: 'https://github.com/spencerwooo',
+                            class: 'button-github'
+                        }
+                    ]
+                }
+            }
+        },
+        methods: {
+            getYear() {
+                return new Date().getFullYear()
+            }
+
+
+        }
     }
-  },
-  methods: {
-    getYear() {
-      return new Date().getFullYear()
-    }
-  }
-}
 </script>
 
 <style>
-@font-face {
-  font-family: 'Product Sans';
-  src: url('assets/ProductSans-Regular.woff2') format('woff2'),
-    url('assets/ProductSans-Regular.woff') format('woff');
-  font-weight: normal;
-  font-style: normal;
-}
+    @font-face {
+        font-family: 'Product Sans';
+        src: url('assets/ProductSans-Regular.woff2') format('woff2'),
+        url('assets/ProductSans-Regular.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
 
-.particles {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 90%;
-  z-index: -9999;
-}
+    .particles {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 90%;
+        z-index: -9999;
+    }
 
-html,
-body {
-  margin: 0;
-  padding: 0;
-}
+    html,
+    body {
+        margin: 0;
+        padding: 0;
+    }
 
-body {
-  border-top: #0f4c81 5px solid;
-}
+    body {
+        border-top: #0f4c81 5px solid;
+    }
 
-.animation-fade-in {
-  -webkit-animation: fade-in-bottom 0.4s cubic-bezier(0.39, 0.575, 0.565, 1)
-    both;
-  animation: fade-in-bottom 0.4s cubic-bezier(0.39, 0.575, 0.565, 1) both;
-}
+    .animation-fade-in {
+        -webkit-animation: fade-in-bottom 0.4s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+        animation: fade-in-bottom 0.4s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+    }
 
-#app {
-  font-family: 'Product Sans', 'Avenir Next', -apple-system, BlinkMacSystemFont,
-    Segoe UI, Helvetica, Arial, 'PingFang SC', 'Noto Sans CJK SC', 'Microsoft YaHei',
-    sans-serif, Apple Color Emoji, Segoe UI Emoji;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #24292e;
-  -webkit-animation: fade-in-bottom 0.4s cubic-bezier(0.39, 0.575, 0.565, 1)
-    both;
-  animation: fade-in-bottom 0.4s cubic-bezier(0.39, 0.575, 0.565, 1) both;
-}
+    #app {
+        font-family: 'Product Sans', 'Avenir Next', -apple-system, BlinkMacSystemFont,
+        Segoe UI, Helvetica, Arial, 'PingFang SC', 'Noto Sans CJK SC', 'Microsoft YaHei',
+        sans-serif, Apple Color Emoji, Segoe UI Emoji;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #24292e;
+        -webkit-animation: fade-in-bottom 0.4s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+        animation: fade-in-bottom 0.4s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+    }
 
-.container {
-  text-align: center;
-  margin: 0px auto 0 auto;
-  max-width: 664px;
-  padding: 0px 20px 40px 20px;
-}
+    .container {
+        text-align: center;
+        margin: 0px auto 0 auto;
+        max-width: 664px;
+        padding: 0px 20px 40px 20px;
+    }
 
-#main-navbar {
-  padding-left: 15px;
-  padding-right: 5px;
-}
+    #main-navbar {
+        padding-left: 15px;
+        padding-right: 5px;
+    }
 
-#main-navbar .button-github {
-  background: white;
-  color: #24292e;
-  border: 1px solid #0f4c81;
-  /* border-radius: 50px; */
-  transition: 0.3s;
-}
+    #main-navbar .button-github {
+        background: white;
+        color: #24292e;
+        border: 1px solid #0f4c81;
+        /* border-radius: 50px; */
+        transition: 0.3s;
+    }
 
-#main-navbar .button-github:hover {
-  background: #0f4c81;
-  color: #ffffff;
-  -webkit-box-shadow: 0px 10px 40px -10px #0f4c81;
-  -moz-box-shadow: 0px 10px 40px -10px #0f4c81;
-  box-shadow: 0px 10px 40px -10px #0f4c81;
-}
+    #main-navbar .button-github:hover {
+        background: #0f4c81;
+        color: #ffffff;
+        -webkit-box-shadow: 0px 10px 40px -10px #0f4c81;
+        -moz-box-shadow: 0px 10px 40px -10px #0f4c81;
+        box-shadow: 0px 10px 40px -10px #0f4c81;
+    }
 
-.vnb__collapse-button img {
-  width: 24px;
-  height: 24px;
-  padding-bottom: 3px;
-}
+    .vnb__collapse-button img {
+        width: 24px;
+        height: 24px;
+        padding-bottom: 3px;
+    }
 
-.vnb__menu-options--right {
-  margin-left: 0;
-  padding-left: 10px;
-}
+    .vnb__menu-options--right {
+        margin-left: 0;
+        padding-left: 10px;
+    }
 
-.vnb__popup__top__close-button {
-  padding-top: 10px;
-}
+    .vnb__popup__top__close-button {
+        padding-top: 10px;
+    }
 
-.link {
-  padding: 0px 10px 0 10px;
-  cursor: pointer;
-  -webkit-transition: color 0.2s ease-in;
-  transition: color 0.2s ease-in;
-}
+    .link {
+        padding: 0px 10px 0 10px;
+        cursor: pointer;
+        -webkit-transition: color 0.2s ease-in;
+        transition: color 0.2s ease-in;
+    }
 
-.link a {
-  text-decoration: none;
-  font-weight: 500;
-  color: #595959;
-  font-size: 1rem;
-}
+    .link a {
+        text-decoration: none;
+        font-weight: 500;
+        color: #595959;
+        font-size: 1rem;
+    }
 
-.vnb__popup__bottom__custom-section .link {
-  padding-bottom: 10px;
-}
+    .vnb__popup__bottom__custom-section .link {
+        padding-bottom: 10px;
+    }
 
-#avatar {
-  max-width: 120px;
-  max-height: 120px;
-  border-radius: 60px;
-  margin-top: 40px;
-}
+    #avatar {
+        max-width: 120px;
+        max-height: 120px;
+        border-radius: 60px;
+        margin-top: 40px;
+    }
 
-.header-image {
-  max-width: 120px;
-  max-height: 120px;
-  margin-top: 40px;
-}
+    .header-image {
+        max-width: 120px;
+        max-height: 120px;
+        margin-top: 40px;
+    }
 
-.article {
-  text-align: left;
-}
+    .article {
+        text-align: left;
+    }
 
-h1 {
-  font-weight: 700;
-  font-size: 28px;
-}
+    h1 {
+        font-weight: 700;
+        font-size: 28px;
+    }
 
-h2 {
-  font-weight: 400;
-  font-size: 20px;
-  padding: 20px 0;
-}
+    h2 {
+        font-weight: 400;
+        font-size: 20px;
+        padding: 20px 0;
+    }
 
-hr {
-  width: 100px;
-  margin: 60px 0 0 0;
-  border-top: solid 3px #24292e;
-}
+    hr {
+        width: 100px;
+        margin: 60px 0 0 0;
+        border-top: solid 3px #24292e;
+    }
 
-p,
-span {
-  color: #666666;
-}
+    p,
+    span {
+        color: #666666;
+    }
 
-#top-hr {
-  width: 100px;
-  margin: 60px auto;
-  border-top: solid 3px #0f4c81;
-}
+    #top-hr {
+        width: 100px;
+        margin: 60px auto;
+        border-top: solid 3px #0f4c81;
+    }
 
-.social-media-card {
-  display: flex;
-  align-items: center;
-  margin: 20px 0;
-}
+    .social-media-card {
+        display: flex;
+        align-items: center;
+        margin: 20px 0;
+    }
 
-.social-media-card .media-icon {
-  width: 20px;
-  height: 20px;
-  padding-right: 10px;
-}
+    .social-media-card .media-icon {
+        width: 20px;
+        height: 20px;
+        padding-right: 10px;
+    }
 
-.container a {
-  text-decoration: none;
-  color: #0f4c81;
-}
+    .container a {
+        text-decoration: none;
+        color: #0f4c81;
+    }
 
-.container a:hover {
-  border-bottom: #0f4c81 solid 1px;
-}
+    .container a:hover {
+        border-bottom: #0f4c81 solid 1px;
+    }
 
-#others {
-  text-align: left;
-}
+    #others {
+        text-align: left;
+    }
 
-#footer {
-  padding: 20px 20px;
-  background-color: #24292e;
-}
+    #footer {
+        padding: 20px 20px;
+        background-color: #24292e;
+    }
 
-#footer-text {
-  margin: 0px auto;
-  max-width: 664px;
-  color: #ffffff;
-}
+    #footer-text {
+        margin: 0px auto;
+        max-width: 664px;
+        color: #ffffff;
+    }
 
-#footer-text a,
-#busuanzi_value_site_pv,
-#footer-copyright {
-  color: #ffffff;
-}
+    #footer-text a,
+    #busuanzi_value_site_pv,
+    #busuanzi_value_site_uv,
+    #footer-copyright {
+        color: #ffffff;
+    }
 
-/**
- * ----------------------------------------
- * animation fade-in-bottom
- * ----------------------------------------
- */
-@-webkit-keyframes fade-in-bottom {
-  0% {
-    -webkit-transform: translateY(50px);
-    transform: translateY(50px);
-    opacity: 0;
-  }
-  100% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    opacity: 1;
-  }
-}
-@keyframes fade-in-bottom {
-  0% {
-    -webkit-transform: translateY(50px);
-    transform: translateY(50px);
-    opacity: 0;
-  }
-  100% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    opacity: 1;
-  }
-}
+    /**
+     * ----------------------------------------
+     * animation fade-in-bottom
+     * ----------------------------------------
+     */
+    @-webkit-keyframes fade-in-bottom {
+        0% {
+            -webkit-transform: translateY(50px);
+            transform: translateY(50px);
+            opacity: 0;
+        }
+        100% {
+            -webkit-transform: translateY(0);
+            transform: translateY(0);
+            opacity: 1;
+        }
+    }
+
+    @keyframes fade-in-bottom {
+        0% {
+            -webkit-transform: translateY(50px);
+            transform: translateY(50px);
+            opacity: 0;
+        }
+        100% {
+            -webkit-transform: translateY(0);
+            transform: translateY(0);
+            opacity: 1;
+        }
+    }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,126 +1,125 @@
 <template>
-  <div class="container">
-    <img id="avatar" alt="avatar" src="./../assets/icon.png" />
-    <h1>Jack Wang</h1>
-	<h3>CS精神在读 / 啥都会一点的程序猿 </h3>
-    <p>Status: 爱尔兰留学申请ing.
-	 <a href="https://wangbj.top/404"> [🎯 Progress]</a>
-	</p>
-    <p>Goal: Working on Java and Android, Dreaming to be a Software Engineering.
-      <a href="https://wangbj.top/404"> [🎯 Progress]</a>
-    </p>
+    <div class="container">
+        <img id="avatar" alt="avatar" src="./../assets/icon.png"/>
+        <h1>Jack Wang</h1>
+        <h3>CS精神在读 / 啥都会一点的程序猿 </h3>
+        <p>Status: 爱尔兰留学申请ing.
+            <a href="https://wangbj.top/404"> [🎯 Progress]</a>
+        </p>
+        <p>Goal: Working on Java and Android, Dreaming to be a Software Engineering.
+            <a href="https://wangbj.top/404"> [🎯 Progress]</a>
+        </p>
 
-    <hr id="top-hr" />
-    <h3>🎉 Where to find me:</h3>
-    <div id="card-container">
+        <hr id="top-hr"/>
+        <h3>🎉 Where to find me:</h3>
+        <div id="card-container">
 
-      <Card
-        msg="博客 Blog"
-        backgroundColor="#0083c0"
-        description="📒 立个Flag多写点Blog 🏳️‍🌈"
-        textColor="#FFFFFF"
-        icon="netlify.png"
-        link="https://hugo.wangbj.top"
-      />
-    
-	   <Card
-        msg="GitHub"
-        backgroundColor="#24292E"
-        description="📦 我做过的项目都在这里"
-        textColor="#FFFFFF"
-        icon="github.png"
-        link="https://github.com/skycity233"
-      />
-	  
-	   <Card
-        msg="Hexo Blog"
-        backgroundColor="#ca2c2a"
-        description="🔌 不再维护"
-        textColor="#FFFFFF"
-        icon="hexo.png"
-        link="https://blog.wangbj.top"
-      />
-	  
-    </div>
+            <Card
+                    msg="博客 Blog"
+                    backgroundColor="#0083c0"
+                    description="📒 立个Flag多写点Blog 🏳️‍🌈"
+                    textColor="#FFFFFF"
+                    icon="netlify.png"
+                    link="https://hugo.wangbj.top"
+            />
 
-    <div id="others">
-      <hr />
-      <h3>你还可以这样找到我：</h3>
-      <div class="social-media">
-        <div class="social-media-card">
-          <img class="media-icon" src="@/assets/weibo.png" alt="weibo" />
-          <span>
+            <Card
+                    msg="GitHub"
+                    backgroundColor="#24292E"
+                    description="📦 我做过的项目都在这里"
+                    textColor="#FFFFFF"
+                    icon="github.png"
+                    link="https://github.com/skycity233"
+            />
+
+            <Card
+                    msg="Hexo Blog"
+                    backgroundColor="#ca2c2a"
+                    description="🔌 不再维护"
+                    textColor="#FFFFFF"
+                    icon="hexo.png"
+                    link="https://blog.wangbj.top"
+            />
+
+        </div>
+
+        <div id="others">
+            <hr/>
+            <h3>你还可以这样找到我：</h3>
+            <div class="social-media">
+                <div class="social-media-card">
+                    <img class="media-icon" src="@/assets/weibo.png" alt="weibo"/>
+                    <span>
             Weibo:
             <a href="https://weibo.com/2972294092">@SkyCity___</a>
           </span>
-        </div>
-        <div class="social-media-card">
-          <img class="media-icon" src="@/assets/twitter.png" alt="twitter" />
-          <span>
+                </div>
+                <div class="social-media-card">
+                    <img class="media-icon" src="@/assets/twitter.png" alt="twitter"/>
+                    <span>
             Twitter:
             <a href="https://twitter.com/w1197274057">@w1197274057</a>
           </span>
-        </div>
-        <div class="social-media-card">
-          <img class="media-icon" src="@/assets/gmail.png" alt="gmail" />
-          <span>
+                </div>
+                <div class="social-media-card">
+                    <img class="media-icon" src="@/assets/gmail.png" alt="gmail"/>
+                    <span>
             Gmail:
             <a href="mailto: realJackWang@gmail.com">realJackWang@gmail.com</a>
           </span>
+                </div>
+            </div>
+
+
         </div>
-      </div>
+        <div id="others">
+            <hr/>
+            <h3>友情链接：(No Sort)</h3>
 
-  
-
-    </div>
-	<div id="others">
-		<hr />
-		<h3>友情链接：</h3>
-		
-		 <div class="social-media-card">
-          <img class="media-icon" src="@/assets/chongxin.png" alt="chongxin" />
-          <span>
+            <div class="social-media-card">
+                <img class="media-icon" src="@/assets/chongxin.png" alt="chongxin"/>
+                <span>
             山东大学·崇新学堂:&nbsp;
             <a href="http://cxxt.wesdu.cn/">cxxt.wesdu.cn</a>
 			&nbsp;
 			(My College)
           </span>
-        </div>
-		
-		
-		 <div class="social-media-card">
-          <img class="media-icon" src="@/assets/spencerwoo.jpg" alt="spencerwoo" />
-          <span>
+            </div>
+
+
+            <div class="social-media-card">
+                <img class="media-icon" src="@/assets/spencerwoo.jpg" alt="spencerwoo"/>
+                <span>
             Spencer Woo:&nbsp;
             <a href="https://spencerwoo.com">spencerwoo.com</a>
 			 &nbsp;
 			(Special Thanks, Learnt lots of new-tech from his blog.)
 			
           </span>
-        </div>
-		
-		 <div class="social-media-card">
-          <img class="media-icon" src="http://huangyz.name/assets/images/blog-black.svg" alt="huangyz" />
-          <span>
+            </div>
+
+            <div class="social-media-card">
+                <img class="media-icon" src="http://huangyz.name/assets/images/blog-black.svg" alt="huangyz"/>
+                <span>
             Yizheng Huang:&nbsp;
             <a href="http://huangyz.name/">huangyz.name</a>
 			 &nbsp;
 			(Special Thanks, His is my example.)
 			
           </span>
+            </div>
+
         </div>
-	
-	</div>
-	
-  </div>
+
+    </div>
 </template>
 
 <script>
-import Card from '@/components/Card.vue'
+    import Card from '@/components/Card.vue'
 
-export default {
-  components: {
-    Card
-  }
-}
+    export default {
+        components: {
+            Card
+        }
+    }
 </script>
